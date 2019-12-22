abeb2dd23331bcf1f300ae8946418089157c7b04
Bastiaan Veldjesgraaf <12785697+veld8@users.noreply.github.com>
2019-12-22 18:41:47 +0100

    Markdown (with changes)

--- markdown/lib/markdown.ex
+++ markdown/lib/markdown.ex
@@ -12,68 +12,28 @@ defmodule Markdown do
   """
   @spec parse(String.t()) :: String.t()
   def parse(m) do
-    patch(Enum.join(Enum.map(String.split(m, "\n"), fn t -> process(t) end)))
+    m
+    |> String.split("\n")
+    |> Enum.map_join(fn l -> process_line(l) end)
+    |> parse_content()
+    |> join_list_items()
   end
 
-  defp process(t) do
-    if String.starts_with?(t, "#") || String.starts_with?(t, "*") do
-      if String.starts_with?(t, "#") do
-        enclose_with_header_tag(parse_header_md_level(t))
-      else
-        parse_list_md_level(t)
-      end
-    else
-      enclose_with_paragraph_tag(String.split(t))
-    end
+  defp process_line("#" <> l), do: parse_header(l, 1)
+  defp process_line("* " <> l), do: "<li>#{l}</li>"
+  defp process_line(l), do: "<p>#{l}</p>"
+
+  defp parse_header(" " <> l, acc), do: "<h#{acc}>#{l}</h#{acc}>"
+  defp parse_header("#" <> l, acc), do: parse_header(l, acc + 1)
+
+  defp parse_content(l) do
+    l |> parse_bold() |> parse_italic()
   end
 
-  defp parse_header_md_level(hwt) do
-    [h | t] = String.split(hwt)
-    {to_string(String.length(h)), Enum.join(t, " ")}
-  end
+  defp parse_bold(l), do: String.replace(l, ~r/__(.*)__/, "<strong>\\g{1}</strong>")
+  defp parse_italic(l), do: String.replace(l, ~r/_(.*)_/, "<em>\\g{1}</em>")
 
-  defp parse_list_md_level(l) do
-    t = String.split(String.trim_leading(l, "* "))
-    "<li>" <> join_words_with_tags(t) <> "</li>"
-  end
-
-  defp enclose_with_header_tag({hl, htl}) do
-    "<h" <> hl <> ">" <> htl <> "</h" <> hl <> ">"
-  end
-
-  defp enclose_with_paragraph_tag(t) do
-    "<p>#{join_words_with_tags(t)}</p>"
-  end
-
-  defp join_words_with_tags(t) do
-    Enum.join(Enum.map(t, fn w -> replace_md_with_tag(w) end), " ")
-  end
-
-  defp replace_md_with_tag(w) do
-    replace_suffix_md(replace_prefix_md(w))
-  end
-
-  defp replace_prefix_md(w) do
-    cond do
-      w =~ ~r/^#{"__"}{1}/ -> String.replace(w, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      w =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(w, ~r/_/, "<em>", global: false)
-      true -> w
-    end
-  end
-
-  defp replace_suffix_md(w) do
-    cond do
-      w =~ ~r/#{"__"}{1}$/ -> String.replace(w, ~r/#{"__"}{1}$/, "</strong>")
-      w =~ ~r/[^#{"_"}{1}]/ -> String.replace(w, ~r/_/, "</em>")
-      true -> w
-    end
-  end
-
-  defp patch(l) do
-    String.replace_suffix(
-      String.replace(l, "<li>", "<ul>" <> "<li>", global: false),
-      "</li>",
-      "</li>" <> "</ul>"
-    )
+  defp join_list_items(s) do
+    String.replace(s, ~r/(\<li\>.*\<\/li\>)/, "<ul>\\g{1}</ul>")
   end
 end
